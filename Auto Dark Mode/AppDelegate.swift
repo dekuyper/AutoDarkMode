01c2bd6f27a8476ec6f528da0a36ae841c2711d6
Nick <nicu.oprisan@gmail.com>
2018-10-02 10:40:33 +0300

    Initial Commit

---
+++ Auto Dark Mode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Auto Dark Mode
+//
+//  Created by Nicolae Oprisan on 02/10/2018.
+//  Copyright © 2018 OakGrove Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
