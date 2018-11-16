6ae7d236c9362957611abe7dbbb2c8cd5cf64112
Nick <nicu.oprisan@gmail.com>
2018-11-16 15:12:52 +0200

    Tinkering with the notifications.

--- Auto Dark Mode/NotificationHandler.swift
+++ Auto Dark Mode/NotificationHandler.swift
@@ -14,19 +14,19 @@ class NotificationHandler: AppManagedObject, AppManagerDelegate, UNUserNotificat
     let categoryId = "DARK_MODE_SWITCH"
     let notificationCenter = UNUserNotificationCenter.current()
     
-    func scheduleNotification() {
+    func scheduleNotification(switchTo: String) {
         // Content
         let content = UNMutableNotificationContent()
         content.title = "Auto Dark Mode"
-        content.body = "Dark Mode will be Disabled in 15 seconds."
+        content.body = "Dark Mode will be \(switchTo) in 15 seconds."
         content.categoryIdentifier = categoryId
-        content.userInfo = ["SWITCH_TO": "Disable"]
+        content.userInfo = ["SWITCH_TO": switchTo]
 
         // Trigger time
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
 
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-        notificationCenter.add(request)
+        notificationCenter.add(request){ (error: Error?) in if let error = error { print(error) } }
     }
     
     // Location Handler notifications
@@ -66,10 +66,11 @@ class NotificationHandler: AppManagedObject, AppManagerDelegate, UNUserNotificat
             // Enable or disable features based on authorization.
             if granted {
                 print("Notifications permission granted.")
-                //self.declareNotificationTypes()
-                self.scheduleNotification()
+                self.declareNotificationTypes()
+                self.scheduleNotification(switchTo: "Disabled")
             } else {
                 print("Notifications permission denied.")
+                print("\(error)")
             }
         }
     }
